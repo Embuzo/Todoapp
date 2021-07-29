f255514f133383450c33bb43758e0ab67685431f
Eemil Kaiturinmäki <eemil.kaiturinmaki@gmail.com>
2021-07-29 16:15:08 +0300

    for heroku

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -1,3 +1,6 @@
+import django_heroku
+import dj_database_url
+from decouple import config
 """
 Django settings for todo_app project.
 
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'todo_app.urls'
@@ -121,7 +125,12 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
